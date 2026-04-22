65549b9d78b0171e74b83bff7ae1112c46bc1850
joaovitorlouzada <joaovitorlouzada@hotmail.com>
2026-04-22 17:32:02 -0300

    Fim da seção 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.c
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200", "http://localhost:61650"));
+
 app.MapControllers();
 
 app.Run();
